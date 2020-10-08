dfccd3afa48f5a7c87307b7f7f1ce8d866089e4e
Omar <55831464+Omario64@users.noreply.github.com>
2020-10-08 18:58:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,67 @@ const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
 const app     = express();
+app.get('/', function(req, res){
+    // let date = req.query.date;
+    // let sign = req.query.sign;
+    // let url = 'https://www.astrology.com/horoscope/daily/' + date + '/' + sign + '.html';
+    let typ = req.query.typ;
+    let lan = req.query.lan;
+    let kommun = req.query.kommun;
 
-app.get('/', function(req, res) {
+    let urlTest = 'https://bensinpriser.nu/stationer/' + typ + '/' + lan + '/' + kommun + '';
 
-  let date = req.query.date.toLowerCase();
-  let sign = req.query.sign.toLowerCase();
 
-  let url = 'https://www.astrology.com/horoscope/daily/' + date + '/' + sign + '.html';
+    request(urlTest, function(error, response, html) {
 
-  // The structure of our request call
-  // The first parameter is our URL
-  // The callback function takes 3 parameters, an error, response status code and the html
-  request(url, function(error, response, html) {
+      // First we'll check to make sure no errors occurred when making the request
+      if (!error) {
 
-    // First we'll check to make sure no errors occurred when making the request
-    if (!error) {
+        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+        var $ = cheerio.load(html);
 
-      // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-      var $ = cheerio.load(html);
+        // Finally, we'll define the variable we're going to capture
+        // We'll be using Cheerio's function to single out the necessary information
+        // using DOM selectors which are normally found in CSS.
+        // var prediction = $('tr').text();
+        // var predictionTest = $('td').each((index, element) => {
+        //   console.log($(element).text());
+        //   });
 
-      // Finally, we'll define the variable we're going to capture
-      // We'll be using Cheerio's function to single out the necessary information
-      // using DOM selectors which are normally found in CSS.
-      var prediction = $('div.daily-horoscope > p').text();
 
-      // And now, the JSON format we are going to expose
-      var json = {
-        date: date,
-        sign: sign,
-        prediction: prediction
-      };
+          const scrapedData = [];
+          $('#price_table > tbody > tr').each((index, element) => {
+              if (index === 0) return true;
+              const tds = $(element).find('td');
+              const station = $(tds[0]).text();
+              const pris = $(tds[1]).text();
+              const tableRow = { station, pris };
+              scrapedData.push(tableRow);
+          });
+          // console.log(scrapedData)
+          res.send(scrapedData)
 
-      // Send the JSON as a response to the client
-      res.send(json);
-    }
 
-  });
+        // And now, the JSON format we are going to expose
+        // var json = {
+        //   lan: lan,
+        //   kommun: kommun,
+        //   typ: typ,
+        //   predictionTest: predictionTest
+        // };
+        // console.log(json)
+        // res.send(json)
+
+        // console.log(prediction);
+        // res.send(prediction)
+
+        // console.log(json);
+        // res.send(json)
+        // Send the JSON as a response to the client
+      }
+
+    });
 
 });
-
 app.listen(process.env.PORT || 5000);
 module.exports = app;
